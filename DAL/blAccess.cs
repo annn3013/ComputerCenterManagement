270625704c55df5aa21f1c7791ac899d25fdafe7
Bach <tonbach41@gmail.com>
2023-04-21 02:57:47 +0700

    Dal

---
+++ DAL/blAccess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using System.Security.Policy;
+using Microsoft.SqlServer.Server;
+
+namespace DAL
+{
+    public  class blAccess:DbAccess
+    {
+        public string themBL2(BienLai bl)
+        {
+            return ThemBL(bl);
+        }
+        public string xoaBL2(string id)
+        {
+            return xoaBL(id);
+        }
+        public string suaBL2(BienLai bl)
+        {
+            return suaBL(bl);
+        }
+        public DataTable loadBL2()
+        {
+            return loadBL();
+        }
+        public DataTable loadBLT2(string mahv)
+        {
+            return loadBLT(mahv);
+        }
+        public string autoBL2()
+        {
+            return autoBL();
+        }
+        public string CheckBL2(string mahv)
+        {
+            return checkBL(mahv);
+        }
+        public string CheckMBL2(string id)
+        {
+            return checkMBL(id);
+        }
+        public DataTable loadBLID2(string id)
+        {
+            return loadBLID(id);
+        }
+    }
+}
---
+++ DAL/hvAccess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace DAL
+{
+    public class hvAccess:DbAccess
+    {
+        public string themHV2(HocVien hv)
+        {
+            return themHV(hv);
+        }
+        public string LayMaCuoi2()
+        {
+            return LayMaCuoi();
+        }
+        public DataTable LoadHV2()
+        {
+            return loadHV();
+        }
+        public DataTable LoadHVT2(string mahv)
+        {
+            return loadHVT(mahv);
+        }
+        public DataTable LoadTHV2(string hoten)
+        {
+            return loadTHV(hoten);
+        }
+        public string TimHocVien2(string mahv)
+        {
+            if (mahv == "")
+            {
+                return "vui lòng nhập mã học viên để tìm kiếm";
+            }
+            return TimHocVien(mahv);
+        }
+        public string capNhatHV2(HocVien hv)
+        {
+            return capNhatHV(hv);
+        }
+        public string xoaHV2(string mahv)
+        { 
+            return xoaHV(mahv);
+        }
+        public string TimHocVienT2(string tenhv)
+        {
+            return TimHocVienT(tenhv);
+        }
+    }
+}
